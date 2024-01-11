3db06e7861e0f88897bafcbb7a697c4135523051
Andrey Plekhanov <ierehon91@gmail.com>
2024-01-11 14:00:29 +0300

    - Добавлено приложение корзины cart
    - Создан класс Cart работы с корзиной в сессии, содержащий методы:
    1) Добавление товара в корзину
    2) Удаление товара из корзины
    3) Сохранение изменений в сеансе
    4) Метод __iter__ для итерирования позиций в корзине и получения товара из БД
    5) Метод __len__ для получения количества позиций в корзине
    6) Метод для подсчёта общей стоимости всех товаров в корзине
    7) Метод очистки сеанса корзины

--- myshop/settings.py
+++ myshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig,'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
